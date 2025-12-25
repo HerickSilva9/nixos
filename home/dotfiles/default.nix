b9a7569f17773254fc90dd5750a8bce365b2e28c
HerickSilva9 <149031628+HerickSilva9@users.noreply.github.com>
2025-12-25 16:33:27 -0300

    Adiciona dotfiles gerenciados pelo home-manager

--- home/dotfiles/default.nix
+++ home/dotfiles/default.nix
@@ -5,6 +5,9 @@
   # Kitty
   home.file.".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "/home/herick/nixos/home/dotfiles/kitty";
 
+  # nvim
+  home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/herick/nixos/home/dotfiles/nvim";
+
   # user-dirs
   home.file = {
       ".config/user-dirs.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/herick/nixos/home/dotfiles/user-dirs/user-dirs.conf";
