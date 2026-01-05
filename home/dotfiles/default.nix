3fa0247deb9dc80f159e1b5f0df1cb85452f3658
HerickSilva9 <149031628+HerickSilva9@users.noreply.github.com>
2026-01-05 08:52:02 -0300

    Remmina files

--- home/dotfiles/default.nix
+++ home/dotfiles/default.nix
@@ -18,6 +18,9 @@ in
   # nvim
   home.file.".config/nvim".source = mkOutOfStore "${home}/nixos/home/dotfiles/nvim";
 
+  # Remmina
+  home.file.".local/share/remmina".source = mkOutOfStore "${home}/nixos/dotfiles/remmina";
+
   # user-dirs
   home.file = {
       ".config/user-dirs.conf".source = mkOutOfStore "${home}/nixos/home/dotfiles/user-dirs/user-dirs.conf";
