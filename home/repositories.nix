7f273a5fb713e98c995412105e5a393b2517f5ef
HerickSilva9 <149031628+HerickSilva9@users.noreply.github.com>
2026-01-10 00:55:58 -0300

    Adiciona repo de wallpapers do Dracula como comentário em home/repositories.nix

--- home/repositories.nix
+++ home/repositories.nix
@@ -2,7 +2,9 @@
 
 {
 
-  # Wallpapers
+  ## Wallpapers
+
+  # orangc's walls, catppuccin-mocha edition
   home.file."imagens/walls-catppuccin-mocha" = {
     source = pkgs.fetchFromGitHub {
       owner = "orangci";
@@ -12,5 +14,17 @@
     };
   recursive = true;
   };
+
+  # Dracula Wallpaper
+  # Awesome wallpapers with the theme and colors of Dracula Theme!
+  # home.file."imagens/DraculaWallpapers" = {
+  #   source = pkgs.fetchFromGitHub {
+  #     owner = "dracula";
+  #     repo = "wallpaper";
+  #     rev = "master";
+  #     sha256 = "sha256-P0MfGkVap8wDd6eSMwmLhvQ4/7Z+pNmgY7O+qt9C1bg";
+  #   };
+  # recursive = true;
+  # };
 
 }
