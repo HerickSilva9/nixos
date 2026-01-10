d156de05a06135c1b6f14c723b0da9400d213079
HerickSilva9 <149031628+HerickSilva9@users.noreply.github.com>
2026-01-09 22:07:22 -0300

    Adiciona possíveis pacotes para exclusão no kde

--- desktop/kde/default.nix
+++ desktop/kde/default.nix
@@ -16,4 +16,27 @@
     catppuccin-kde 
   ];
 
+
+  environment.plasma6.excludePackages = with pkgs; [
+    # kdePackages.aurorae
+    # kdePackages.plasma-browser-integration
+    # kdePackages.plasma-workspace-wallpapers
+    # kdePackages.konsole
+    # kdePackages.kwin-x11
+    # kdePackages.(lib.getBin qttools) # Expose qdbus in PATH
+    # kdePackages.ark
+    kdePackages.elisa
+    # kdePackages.gwenview
+    # kdePackages.okular
+    # kdePackages.kate
+    # kdePackages.ktexteditor # provides elevated actions for kate
+    # kdePackages.khelpcenter
+    # kdePackages.dolphin
+    # kdePackages.baloo-widgets # baloo information in Dolphin
+    # kdePackages.dolphin-plugins
+    # kdePackages.spectacle
+    # kdePackages.ffmpegthumbs
+    # kdePackages.krdp
+  ];
+
 }
