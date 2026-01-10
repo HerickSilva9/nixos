7c4bf88688b586719251355b5beff525dc5f3ac2
HerickSilva9 <149031628+HerickSilva9@users.noreply.github.com>
2026-01-09 22:08:19 -0300

    Adiciona comentários e organiza office/default.nix

--- config/office/default.nix
+++ config/office/default.nix
@@ -3,9 +3,17 @@
 {
   
   environment.systemPackages = with pkgs; [
+    # Comprehensive, professional-quality productivity suite, a variant of openoffice.org
     # libreoffice-qt-fresh
+
+    # Office suite that combines text, spreadsheet and presentation editors allowing to create, view and edit local documents
     # onlyoffice-desktopeditors
+
+    # GNOME Office Spreadsheet
     # gnumeric
-  ];
+
+    # Terminal Excel viewer with an interactive TUI
+    # xleak
+    ];
 
 }
